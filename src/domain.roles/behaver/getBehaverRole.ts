b2281499da07f76e088d953f9c9808d32e2da7ba
Ulad Kasach <uladkasach@gmail.com>
2025-12-22 13:06:46 -0600

    fix(init): init based on rhachet-roles-bhrain

---
+++ src/domain.roles/behaver/getBehaverRole.ts
@@ -0,0 +1,20 @@
+import { Role } from 'rhachet';
+
+export const ROLE_BEHAVER: Role = Role.build({
+  slug: 'bhuild',
+  name: 'Behaver',
+  purpose: 'declare clear, buildable, and testable behaviors',
+  readme: `
+## 📐 Behaver
+
+Used to declare clear and testable behaviors that can be reliably built and verified.
+  `.trim(),
+  traits: [],
+  skills: {
+    dirs: [{ uri: __dirname + '/skills' }],
+    refs: [],
+  },
+  briefs: {
+    dirs: [{ uri: __dirname + '/briefs' }],
+  },
+});
