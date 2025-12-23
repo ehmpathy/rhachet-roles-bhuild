2fd3f9951a7f8be955ec801eac75f76d3ec0adda
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2025-12-23 17:26:26 -0600

    feat(behaver): add skill review.deliverables (#12)

--- rhachet.use.ts
+++ rhachet.use.ts
@@ -2,7 +2,7 @@ import type { InvokeHooks, RoleRegistry } from 'rhachet';
 
 import { getRoleRegistry as getRoleRegistryEhmpathy, getInvokeHooks as getInvokeHooksEhmpathy } from 'rhachet-roles-ehmpathy';
 import { getRoleRegistry as getRoleRegistryBhuild, getInvokeHooks as getInvokeHooksBhuild } from 'rhachet-roles-bhuild';
-// import { getRoleRegistry as getRoleRegistryBhuild, getInvokeHooks as getInvokeHooksBhuild } from 'dist/index.js';
+// import { getRoleRegistry as getRoleRegistryBhuild, getInvokeHooks as getInvokeHooksBhuild } from './dist/index.js';
 
 export const getRoleRegistries = (): RoleRegistry[] => [getRoleRegistryEhmpathy(), getRoleRegistryBhuild()];
 export const getInvokeHooks = (): InvokeHooks[] => [getInvokeHooksEhmpathy(), getInvokeHooksBhuild()];
