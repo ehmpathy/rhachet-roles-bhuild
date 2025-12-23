99cef80beaf467e6cc1248293cbe56f3e6b7bc9d
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2025-12-23 07:26:42 -0600

    fix(behaver): ensure role is resolvable from repo registry (#8)

--- rhachet.use.ts
+++ rhachet.use.ts
@@ -1,7 +1,8 @@
 import type { InvokeHooks, RoleRegistry } from 'rhachet';
 
 import { getRoleRegistry as getRoleRegistryEhmpathy, getInvokeHooks as getInvokeHooksEhmpathy } from 'rhachet-roles-ehmpathy';
-// import { getRoleRegistry as getRoleRegistryEhmpathy, getInvokeHooks as getInvokeHooksEhmpathy } from './dist/index.js';
+import { getRoleRegistry as getRoleRegistryBhuild, getInvokeHooks as getInvokeHooksBhuild } from 'rhachet-roles-bhuild';
+// import { getRoleRegistry as getRoleRegistryBhuild, getInvokeHooks as getInvokeHooksBhuild } from 'dist/index.js';
 
-export const getRoleRegistries = (): RoleRegistry[] => [getRoleRegistryEhmpathy()];
-export const getInvokeHooks = (): InvokeHooks[] => [getInvokeHooksEhmpathy()];
+export const getRoleRegistries = (): RoleRegistry[] => [getRoleRegistryEhmpathy(), getRoleRegistryBhuild()];
+export const getInvokeHooks = (): InvokeHooks[] => [getInvokeHooksEhmpathy(), getInvokeHooksBhuild()];
