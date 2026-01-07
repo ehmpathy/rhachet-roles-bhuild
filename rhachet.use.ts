cdbd067ae93b4a858101e276947b8e8a80323804
Ulad Kasach <uladkasach@gmail.com>
2026-01-07 07:16:01 -0600

    fix(pkg): ensure readmes are bundled with roles

--- rhachet.use.ts
+++ rhachet.use.ts
@@ -1,6 +1,6 @@
 import type { InvokeHooks, RoleRegistry } from 'rhachet';
 
-import { getRoleRegistry as getRoleRegistryBhuild, getInvokeHooks as getInvokeHooksBhuild } from './dist/index.js';
+import { getRoleRegistry as getRoleRegistryBhuild, getInvokeHooks as getInvokeHooksBhuild } from 'rhachet-roles-bhuild';
 import { getRoleRegistry as getRoleRegistryEhmpathy, getInvokeHooks as getInvokeHooksEhmpathy } from 'rhachet-roles-ehmpathy';
 import { getRoleRegistry as getRoleRegistryBhrain, getInvokeHooks as getInvokeHooksBhrain } from 'rhachet-roles-bhrain';
 
